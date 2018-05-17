f41478512ba101a6dea7068c9ab4598430874153
antaresvalle <antaresvll561@gmail.com>
2018-05-17 18:55:40 -0500

    VERSIÓN 0.0.2

--- js/app.js
+++ js/app.js
@@ -5,9 +5,50 @@ console.log(tweet);
 var inputTweet = document.getElementById('textarea-tweet');
 inputTweet.focus();
 
+tweet.disabled = true;
+tweet.style.backgroundColor = "gray";
+tweet.style.cursor = "no-drop";
+
+
+var counter = document.getElementById('contador');
+
+// deshabilitar boton si el input esta vacio
+
+inputTweet.addEventListener('input', function(){
+	tweet.disabled = false;
+	tweet.style.backgroundColor = "#E07692";
+	tweet.style.cursor = "pointer";
+
+	var inputLength = inputTweet.value.length;
+
+	if(inputTweet.value.length == 0){
+		console.log("input vacio");
+		tweet.disabled = true;
+		tweet.style.backgroundColor = "gray";
+		tweet.style.cursor = "no-drop";
+	}else{
+		var limiteTweet = 140;
+
+		var conteoInverso = limiteTweet - inputLength;
+			var textCounter = document.createTextNode(conteoInverso);
+			counter.appendChild(textCounter);
+
+		inputTweet.addEventListener('keydown', function(){
+			textCounter.textContent = '';
+			console.log(textCounter);
+		});
+		
+	}
+
+});
+
+
+
 
 //agregar un evento de click al botón
 tweet.addEventListener('click', function(){
+
+	var emptyCounter = counter.textContent = '';
 	
 	var getTextTweet = inputTweet.value;
 	console.log(getTextTweet);
@@ -30,8 +71,40 @@ tweet.addEventListener('click', function(){
 	tweetContentP.style.padding = "3em";
 	console.log(tweetContent);
 
+	// contar hacia abajo
+
+
 	inputTweet.value = '';
 
 
+
+	// deshabilitar boton si el texto esta vacio, 2da. parte
+
+	inputTweet.addEventListener('focus', function(){
+		tweet.disabled = false;
+		tweet.style.backgroundColor = "#E07692";
+		tweet.style.cursor = "pointer";
+
+		if(inputTweet.value.length == 0){
+			console.log("input vacio");
+			tweet.disabled = true;
+			tweet.style.backgroundColor = "gray";
+			tweet.style.cursor = "no-drop";
+		}else{
+		var limiteTweet = 140;
+
+		var conteoInverso = limiteTweet - inputLength;
+			var counter = document.getElementById('contador');
+			var textCounter = document.createTextNode(conteoInverso);
+			counter.appendChild(textCounter);
+
+		inputTweet.addEventListener('keydown', function(){
+			textCounter.textContent = '';
+			console.log(textCounter);
+		});
+		
+	}
+
+	});
 
 });
